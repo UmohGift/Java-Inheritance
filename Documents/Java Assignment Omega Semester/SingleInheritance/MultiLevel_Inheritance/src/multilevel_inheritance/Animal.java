722e33af2b57fc98c632449ae83cef203d464543
UmohGift <umohenogift@gmail.com>
2025-04-24 20:35:57 +0100

    Add files via upload

---
+++ Documents/Java Assignment Omega Semester/SingleInheritance/MultiLevel_Inheritance/src/multilevel_inheritance/Animal.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package multilevel_inheritance;
+
+/**
+ *
+ * @author Gift Umoh
+ */
+public class Animal {
+    public void nature(){
+        System.out.println("They have a lower IQ than humans");
+    }
+    public void extinct(){
+        System.out.println("They need to be protected from going extinct");
+    }
+}
